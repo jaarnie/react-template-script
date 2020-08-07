bed56765cdb9e617b1e7807cbaea881422b45bea
John Arnold <johnadamarnold@gmail.com>
2020-08-07 17:27:01 +0100

    add test suite to rails api

--- react_template_script.rb
+++ react_template_script.rb
@@ -184,6 +184,11 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry'
+  gem 'factory_bot_rails', '~> 6.1'
+  gem 'shoulda-matchers', '~> 4.3'
+  gem 'faker'
+  gem 'database_cleaner'
+  gem 'rspec-rails', '~> 4.0', '>= 4.0.1'
 end
 
 group :development do
